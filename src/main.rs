cfc44bf83904c4f4ee18787bcc89d683e0c0e8f1
junyiz <junyime@qq.com>
2021-06-03 09:40:45 +0800

    parse port

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ use std::net::TcpStream;
 use std::net::TcpListener;
 use std::net::{Ipv4Addr,Ipv6Addr,SocketAddrV6,SocketAddrV4,Shutdown};
 use std::time::Duration;
+use bytes::Buf;
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:10080").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:1080").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         thread::spawn(move || {
@@ -36,12 +37,13 @@ fn handle_connection(stream: TcpStream) {
     let mut addr_port = String::from("");
     match atyp {
         0x01 => {
-            // ipv4
+            // ipv4: 4bytes + port
             reader.read_exact(&mut buffer[0..6]).unwrap();
             let mut tmp_array: [u8; 4] = Default::default();
             tmp_array.copy_from_slice(&buffer[0..4]);
             let ipv4 = Ipv4Addr::from(tmp_array);
-            let socket = SocketAddrV4::new(ipv4, 0x1BB);
+            let port: u16 = buffer[4..6].as_ref().get_u16();
+            let socket = SocketAddrV4::new(ipv4, port);
             println!("ipv4 port: {} * 256 + {}", buffer[4], buffer[5]);
             addr_port = format!("{}", socket);
             println!("ipv4: {}", addr_port);
@@ -51,20 +53,22 @@ fn handle_connection(stream: TcpStream) {
             reader.read_exact(&mut buffer[0..1]).unwrap();
             let len = buffer[0] as usize;
             reader.read_exact(&mut buffer[0..len + 2]).unwrap();
+            let port: u16 = buffer[len..len + 2].as_ref().get_u16();
             println!("domain port: {} * 256 + {}", buffer[len], buffer[len + 1]);
             if let Ok(addr) = std::str::from_utf8(&buffer[0..len]) {
-                addr_port = format!("{}:{}", addr, 0x1BB);
+                addr_port = format!("{}:{}", addr, port);
             }
             println!("domain: {}", addr_port);
         }
         0x04 => {
-            // ipv6
+            // ipv6: 16bytes + port
             reader.read_exact(&mut buffer[0..18]).unwrap();
             let mut tmp_array: [u8; 16] = Default::default();
             tmp_array.copy_from_slice(&buffer[0..16]);
             let ipv6 = Ipv6Addr::from(tmp_array);
+            let port = buffer[16..18].as_ref().get_u16();
             println!("domain port: {} * 256 + {}", buffer[16], buffer[17]);
-            let socket = SocketAddrV6::new(ipv6, 0x1BB, 0, 0);
+            let socket = SocketAddrV6::new(ipv6, port, 0, 0);
             addr_port = format!("{}", socket);
             println!("ipv6: {}", addr_port);
         }
